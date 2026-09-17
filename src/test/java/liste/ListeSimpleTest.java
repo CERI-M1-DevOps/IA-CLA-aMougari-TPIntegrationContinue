a7bb19e34aaedaf43ad3233b2bd92237f2fb554a
Mougari Abderrahim <1032215692@pfur.ru>
2026-09-17 18:49:11 +0200

    Add assertion to toString test

--- src/test/java/liste/ListeSimpleTest.java
+++ src/test/java/liste/ListeSimpleTest.java
@@ -49,7 +49,8 @@ public class ListeSimpleTest {
         listeATester.ajout(1);
         listeATester.ajout(2);
         listeATester.ajout(3);
-        System.out.println(listeATester);
+        assertEquals("ListeSimple(Noeud(1), Noeud(2), Noeud(3))",
+		     listeATester.toString());
     }
 
     @Test
